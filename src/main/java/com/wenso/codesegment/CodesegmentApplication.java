441fa67e5b6fba08e4bf693ee5fba06c5fe78e44
yuweonsong <yuweonsong@123.com>
2022-07-26 08:12:01 +0800

    排除特定自动配置

--- src/main/java/com/wenso/codesegment/CodesegmentApplication.java
+++ src/main/java/com/wenso/codesegment/CodesegmentApplication.java
@@ -2,8 +2,10 @@ package com.wenso.codesegment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// 排除特定自动配置
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CodesegmentApplication {
 
     public static void main(String[] args) {
